ecb4cac3a7a34d39bb804ba1f8ff947d14e960eb
Nathan Page <nathan.page@samtec.com>
2018-10-10 17:05:12 -0400

    motor driver code truly works

--- src/motor_driver.py
+++ src/motor_driver.py
@@ -1,30 +1,29 @@
 import time
-from machine import Pin
+from machine import Pin, PWM
 
-MOTOR_DIR = 0 # specify the gpio pin of motor dir
+MOTOR_DIR = 4 # specify the gpio pin of motor dir
 # dont use pin 1, it seems to freeze up when doing so
-MOTOR_PWM = 2 # specify the gpio pulse width value
+MOTOR_PWM = 5 # specify the gpio pulse width value
 
 SPRAY_TIME = 1 # timers for spraying duration
 PAUSE_TIME = 0.2
 
-SPRAY_RELEASE = 0 # specify the direction to spray
-SPRAY_PUSH = 1
-
 OFF = 0
 ON = 1
 
-motor_dir_pin = Pin(MOTOR_DIR, Pin.OUT, value=SPRAY_RELEASE) # create motor direction pin
-motor_pwm_pin = Pin(MOTOR_PWM, Pin.OUT, value=OFF) # create motor pulse width modulation
+motor_pin_a = Pin(MOTOR_DIR, Pin.OUT, value=OFF)
+motor_pin_b = Pin(MOTOR_PWM, Pin.OUT, value=OFF)
 
 
 def spray():
-    motor_dir_pin.value(SPRAY_PUSH) # forward direction
-    motor_pwm_pin.value(ON) # turn spray on
-    time.sleep(SPRAY_TIME) # let it push down
-    motor_pwm_pin.value(OFF) # stop spray
-    time.sleep(PAUSE_TIME)
-    motor_dir_pin.value(SPRAY_RELEASE) # forward direction
-    motor_pwm_pin.value(ON)
+    motor_pin_a.on()
+    motor_pin_b.off()
     time.sleep(SPRAY_TIME)
-    motor_pwm_pin.value(OFF)
+    motor_pin_a.off()
+    motor_pin_b.off()
+    time.sleep(PAUSE_TIME)
+    motor_pin_a.off()
+    motor_pin_b.on()
+    time.sleep(SPRAY_TIME)
+    motor_pin_a.off()
+    motor_pin_b.off()
